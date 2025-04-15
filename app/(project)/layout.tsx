4b82a691b741c9866dc44d5d69bba611447e307c
dgeison.peixoto <dgeison.peixoto@gmail.com>
2025-04-15 18:35:16 -0300

    templante saas - autenticacao

---
+++ app/(project)/layout.tsx
@@ -0,0 +1,23 @@
+// import type { Metadata } from "next";
+
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${poppins.className}  bg-gray-400 antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
