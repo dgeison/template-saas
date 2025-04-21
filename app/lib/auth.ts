26f570e074b05527bf82a87970821c52b530c024
dgeison.peixoto <dgeison.peixoto@gmail.com>
2025-04-21 12:52:00 -0300

    integração com o mercado pago

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,11 +1,11 @@
+import { firebaseCert } from "@/app/lib/firebase";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { firebaseCert } from "./firebase";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   adapter: FirestoreAdapter({
     credential: firebaseCert,
   }),
-});
+})
--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,34 +1,21 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
 import "server-only";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-// Verifica se as variáveis de ambiente estão definidas
-if (!process.env.FIREBASE_PRIVATE_KEY_BASE64) {
-  throw new Error("FIREBASE_PRIVATE_KEY_BASE64 is not defined");
-}
-if (!process.env.FIREBASE_PROJECT_ID) {
-  throw new Error("FIREBASE_PROJECT_ID is not defined");
-}
-if (!process.env.FIREBASE_CLIENT_EMAIL) {
-  throw new Error("FIREBASE_CLIENT_EMAIL is not defined");
-}
+const decodeKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64!, "base64").toString("utf-8");
 
-// Decodifica a chave privada e ajusta o formato para PEM
-const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, "base64").toString("utf-8");
-
-// Configura as credenciais do Firebase
 export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: decodedKey,
+  privateKey: decodeKey,
 });
 
-// Inicializa o Firebase apenas se ainda não estiver inicializado
 if (!getApps().length) {
   initializeApp({
     credential: firebaseCert,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
-// Exporta o Firestore
 export const db = getFirestore();
+// export const storage = getStorage().bucket();
