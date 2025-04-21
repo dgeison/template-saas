415403784dcab9a8363ce5ffdf262702566e708f
dgeison.peixoto <dgeison.peixoto@gmail.com>
2025-04-21 02:05:43 -0300

    integração com o stripe com sucesso

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { handleAuth } from "@/app/actions/handle-auth";
 import { auth } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+
 
 export default async function Dashboard() {
   // Estamos no lado do servidor
@@ -28,6 +30,7 @@ export default async function Dashboard() {
           </button>
         </form>
       )}
+      <Link href="/pagamentos">Pagamentos</Link>
     </div>
   );
 }
